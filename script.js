a4a406185ee5ec3809529451198a9b23433884c2
Harry Barnes <145344818+harrydbarnes@users.noreply.github.com>
2025-03-14 12:43:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+setTimeout(() => {
+    document.getElementById("content").innerHTML = `
+        <h1>Everything isn't as it seems.</h1>
+        <h2>See you soon!</h2>
+    `;
+}, 20000); // 20 seconds
 
